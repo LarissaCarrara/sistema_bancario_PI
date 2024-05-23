28d72b7411eb56b8d98cc4f95998d662c9582113
LarissaCarrara <larissa.carrara@hotmail.com>
2024-05-22 21:35:33 -0300

    configurando controller

--- web/back/src/controllers/bank.controller.js
+++ web/back/src/controllers/bank.controller.js
@@ -32,56 +32,13 @@ const listardados = async (req, res) => {
     else {
         res.status(400).json({ erro: "Clientes não encontrados" }).end();
     }
-
-
 }
 
-const filtrar = (req, res) => {
-    const { categoriaid } = req.params;
-
-    //    let query = `SELECT restaurante.id, restaurante.nome, restaurante.endereco, categoria.nome as categoria FROM restaurante INNER JOIN categoria ON restaurante.categoriaid = categoria.id WHERE restaurante.categoriaid = ${categoriaid}`
-    //     //SELECT * FROM restaurante INNER JOIN categoria ON restaurante.categoriaid = categoria.id
-    //    con.query(query, (err,response) => {
-    //     if(err == undefined){
-    //         res.status(200).json(response).end();
-    //     }else{
-    //         res.status(400).json(err).end();
-    //     }
-// })
-
-}
-
-const categoria = (req, res) => {
-    let query = "SELECT * FROM categoria";
-
-    con.query(query, (err, response) => {
-        if (err == undefined) {
-            res.status(200).json(response).end();
-        } else {
-            res.status(400).json(err).end();
-        }
-    })
-}
-
-const listarCardapio = (req, res) => {
-    let query = "SELECT cardapio.restauranteid, cardapio.descricao, cardapio.valor, restaurante.nome FROM cardapio INNER JOIN restaurante ON restaurante.id = cardapio.restauranteid";
-
-
-    con.query(query, (err, response) => {
-        if (err == undefined) {
-            res.status(200).json(response).end();
-        } else {
-            res.status(400).json(err).end();
-        }
-    })
-}
 
 module.exports = {
     login,
     listardados,
-    filtrar,
-    categoria,
-    listarCardapio
+
 }
 
 //200-deu certo 201 - criou  202- aceito 400-erro 404- não encontrado 500-erro mto grave- tabela http
--- web/back/src/routes/bank.route.js
+++ web/back/src/routes/bank.route.js
@@ -3,14 +3,7 @@ const router = express.Router();
 
 const bank = require('../controllers/bank.controller');
 
-
- router.post('/login', bank.login);
+router.post("/login", bank.login)
  router.get('/listardados', bank.listardados);
- router.get('/listar/:categoriaid', bank.filtrar);
- router.get('/listar/cardapio/:categoriaid', bank.listarCardapio);
- router.get('/categoria', bank.categoria);
- 
-// router.put('/update', bank.alterar);
-// router.delete('/del/:id', bank.excluir);
 
 module.exports = router
