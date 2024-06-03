5ca9813b6365075a36d554fc72a440a0f2899299
Henrique William <os.henriquewiliiam@gmail.com>
2024-06-02 22:48:11 -0300

    tela pix finalizado (ligar com o back-end)

--- web/front/pix/pix.js
+++ web/front/pix/pix.js
@@ -1,8 +1,10 @@
 // const { url } = require("inspector");
 
+const saldo__valor = document.querySelector(".saldo__reais");
+const input = document.querySelector(".pix__dinheiro");
+
+
 const cpfPix = document.querySelector('#cpfPix');
-
-
 
 function fazerPix() {
     console.log("entrei")
@@ -18,14 +20,13 @@ function fazerPix() {
         console.log(data)
         if (data.erro != "Cliente não encontrado") {
             console.log('CPF válido');
-            window.location.href = "./Home";
+            window.location.href = "./pagamento.html";
         } else {
             console.log('CPF inválido');
         }   
     })
     .catch(error => console.error('Erro:', error));
 }
-
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const pagarScreen = document.querySelector('.pix__pagar');
@@ -41,3 +42,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
         window.location.href = './receber.html';
     });
 });
+
+function ocultar() {
+    saldo__valor.classList.toggle('sumir')
+}
+
+function resizeInput(input) {
+    const minWidth = 70; // Minimum width in pixels
+    const characterWidth = 10; // Approximate width of a character in pixels
+
+    // Calculate the new width based on input length
+    const newWidth = minWidth + (input.value.length * characterWidth);
+    input.style.width = newWidth + "px";
+}
